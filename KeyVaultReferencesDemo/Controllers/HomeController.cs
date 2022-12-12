b3b2ee37485edc5b17e2b3a8d5be852ad0b1fc7e
kolosovpetro <kolosovp94@gmail.com>
2022-12-12 22:29:49 +0100

    test values added to appsettings | infrastructure is up

--- KeyVaultReferencesDemo/Controllers/HomeController.cs
+++ KeyVaultReferencesDemo/Controllers/HomeController.cs
@@ -7,14 +7,22 @@ namespace KeyVaultReferencesDemo.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IConfiguration _configuration;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
     {
         _logger = logger;
+        _configuration = configuration;
     }
 
     public IActionResult Index()
     {
+        var pass1 = _configuration["Password1"];
+        var pass2 = _configuration["Password2"];
+
+        ViewBag.Password1 = pass1;
+        ViewBag.Password2 = pass2;
+        
         return View();
     }
 
